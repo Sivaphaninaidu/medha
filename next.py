dd729e40d588420d0f44813290a8c078b3fee558
phani <sivaphaninaidu999@gmail.com>
2026-03-11 13:11:52 +0530

    small change

---
+++ next.py
@@ -0,0 +1,9 @@
+# Write a program that asks for a person's name, age, city, and favorite color. Format and display this information in a neat way using f-strings.
+
+
+name =input("enter the name : ")
+age =int(input("enter the age: "))
+city =input("enter the city: ")
+favorite_color =input("enter the favorite_color : ")
+sentence =f"my self i am {name} my age is {age} and im staying at the {city} and my favorite color is {favorite_color}"
+print(sentence)
