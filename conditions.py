72fdc46edea0b8b6b9e4e414f88b26169793d800
phani <sivaphaninaidu999@gmail.com>
2026-03-16 11:35:41 +0530

    new program

---
+++ conditions.py
@@ -0,0 +1,27 @@
+
+# // Asks the user to enter a number from 1 to 7
+
+# // Uses conditionals to display the corresponding day of the week
+
+# // Handles invalid inputs with an appropriate message
+
+
+
+number = int(input("enter the number: "))
+if number == 1:
+    print("sunday")
+elif number == 2:
+    print("monday")
+elif number == 3:
+    print("tuesday")
+elif number == 4:
+    print("wednesday")
+elif number == 5:
+    print("thursday")
+elif number == 6:
+    print("friday")
+elif number == 7:
+    print("saturday")
+else :
+    print("this is put of the week")
+
